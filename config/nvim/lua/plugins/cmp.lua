1f71a8bd3483152caa9b021f273d14d473e2a6bd
wellatleastitried <jackson1swindell@gmail.com>
2025-10-27 11:42:26 -0500

    New updates to configs

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -2,13 +2,10 @@ return {
 	"hrsh7th/nvim-cmp",
 	ops = function()
 		local cmp = require("cmp")
-		return {
-			enabled = false,
-			mapping = cmp.mapping.preset.insert({
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-CR>"] = cmp.mapping.abort(),
-				["<C-Space-CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
-		}
+		cmp.setup({
+			completion = {
+				autocomplete = false,
+			},
+		})
 	end,
 }
