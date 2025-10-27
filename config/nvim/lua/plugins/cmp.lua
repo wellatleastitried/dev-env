2e7e63200223f95be4daf89caec6b6b2a96a2b41
wellatleastitried <jackson1swindell@gmail.com>
2025-10-27 13:50:53 -0500

    New updates to configs

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -1,4 +1,22 @@
 return {
-	"hrsh7th/nvim-cmp",
-	enabled = false,
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			local cmp = require("cmp")
+
+			opts.experimental = {
+				ghost_text = false,
+			}
+
+			opts.mapping = vim.cmp.mapping.preset.insert({
+				["<CR>"] = function(fallback)
+					fallback()
+				end,
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					fallback()
+				end),
+			})
+		end,
+	},
 }
