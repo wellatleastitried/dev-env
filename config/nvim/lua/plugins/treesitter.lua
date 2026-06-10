4ce9301cd1d5538d064c9e69101123688c92f7df
Jackson Swindell <jackson1swindell@gmail.com>
2026-06-10 15:00:03 -0500

    New updates to configs

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,36 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = {
-				"javascript",
-				"typescript",
-				"ruby",
-				"python",
-				"java",
-				"perl",
-				"bash",
-				"c",
-				"lua",
-				"query",
-				"markdown",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-            },
-            require('nvim-treesitter.configs').setup {
-                rainbow = {
-                    enable = true,
-                    -- list of languages you want to disable the plugin for
-                    disable = { 'jsx', 'cpp' },
-                    -- Which query to use for finding delimiters
-                    query = 'rainbow-parens',
-                    -- Highlight the entire buffer all at once
-                    strategy = require('ts-rainbow').strategy.global,
-                }
-            }
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        dependencies = {
+            "HiPhish/nvim-ts-rainbow2",
         },
-	},
+        opts = function(_, opts)
+            opts.ensure_installed = {
+                "javascript",
+                "typescript",
+                "ruby",
+                "python",
+                "java",
+                "perl",
+                "bash",
+                "c",
+                "lua",
+                "query",
+                "markdown",
+            }
+            opts.sync_install = false
+            opts.auto_install = true
+            opts.highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = true,
+            }
+            opts.rainbow = {
+                enable = true,
+                disable = { "jsx", "cpp" },
+                query = "rainbow-parens",
+                strategy = require("ts-rainbow").strategy.global,
+            }
+        end,
+    },
 }
