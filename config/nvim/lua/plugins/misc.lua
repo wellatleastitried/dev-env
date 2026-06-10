e064293dc46c3c5ad35f0d5339863c50ce8febaa
Jackson Swindell <jackson1swindell@gmail.com>
2026-06-10 13:55:13 -0500

    New updates to configs

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -1,10 +1,31 @@
 return {
-  {
-    "github/copilot.vim",
-    event = "VeryLazy",
-  },
-  {
-    "tpope/vim-dispatch",
-    cmd = { "Dispatch", "Make", "Focus", "Start" },
-  },
+    {
+        "github/copilot.vim",
+        event = "VeryLazy",
+    },
+    {
+        "tpope/vim-dispatch",
+        cmd = { "Dispatch", "Make", "Focus", "Start" },
+    },
+    {
+        "monkoose/nvlime",
+        dependencies = {
+            "monkoose/parsley",
+        },
+        ft = { "lisp" },
+
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "lisp",
+                desc = "Banner reminder to start the Nvlime server",
+                callback = function()
+                    vim.notify(
+                        "Lisp file detected.\nPress <leader>rr to start the Nvlime server!",
+                        vim.log.levels.INFO,
+                        { title = "Nvlime Setup" }
+                    )
+                end,
+            })
+        end,
+    },
 }
