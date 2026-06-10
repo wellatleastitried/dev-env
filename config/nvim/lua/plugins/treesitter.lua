ba753e1945c08604cc0acc717cac1647e8fc4f15
Jackson Swindell <jackson1swindell@gmail.com>
2026-06-10 13:41:26 -0500

    New updates to configs

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -20,7 +20,7 @@ return {
 			auto_install = true,
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = false,
+				additional_vim_regex_highlighting = true,
 			},
 		},
 	},
