cd505613e5f665eb25709bd449a67a804145c1bc
Jackson Swindell <jackson1swindell@gmail.com>
2026-08-31 16:29:19 -0500

    New updates to configs

--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -2,10 +2,14 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
+    opts = {
+        keymaps = {
+            -- These need to freed because they are harpoon keybinds
+            ["<C-h>"] = false,
+            ["<C-l>"] = false,
+            ["<C-r>"] = "actions.refresh",
+        },
+    },
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
 }
