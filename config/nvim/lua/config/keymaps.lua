b81fdbf1510edc3a715155fcbf369935d4582ca9
Jackson Swindell <jackson1swindell@gmail.com>
2026-08-31 16:16:59 -0500

    New updates to configs

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,10 +1,14 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open file explorer" })
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>cc", '"+yy', { desc = "Copy line to system clipboard" })
 vim.keymap.set("v", "<leader>cc", '"+y', { desc = "Copy selection to system clipboard" })
 vim.keymap.set("n", "<leader>pc", '"*p', { desc = "Paste from system clipboard" })
 
+--[[
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
 vim.keymap.set("n", "<leader>E", vim.cmd.Ex, { desc = "Open file explorer" })
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex, { desc = "Open file explorer" })
+]]
 
 vim.keymap.del("n", "s")
+
--- config/nvim/lua/plugins/oil.lua
+++ config/nvim/lua/plugins/oil.lua
@@ -8,5 +8,4 @@ return {
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
-    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 }
