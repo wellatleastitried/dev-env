ed036375143e11c046cdc27eccb7afe49f455315
Jackson Swindell <jackson1swindell@gmail.com>
2026-09-06 14:19:00 -0500

    New updates to configs

---
+++ config/nvim/lua/plugins/bitwise-visualizer.lua
@@ -0,0 +1,9 @@
+return {
+  "wellatleastitried/bitwise.nvim",
+  main = "bitwise-visualizer",
+  event = "VeryLazy",
+  opts = {
+    -- everything is optional; see the configuration reference
+    width = "auto",
+  },
+}
