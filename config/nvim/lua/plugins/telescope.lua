8a908c437ac8fb036673fa9b65dcf220f6881368
Jackson Swindell <jackson1swindell@gmail.com>
2026-09-06 16:47:27 -0500

    New updates to configs

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
+    branch = "master",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = {
       { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find files" },
